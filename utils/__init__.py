0621c10479fbf27afffdf84fa0cce4ee87b0ac0e
OraneD <orane.dufour@gmail.com>
2026-02-04 14:55:45 +0100

    fix import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .data_io import read_kaldi_format, save_kaldi_format, parse_yaml, save_yaml, write_table, load_wav_from_scp
-from .compute_avg_embs import avg_embs
+from .compute_avg_embs import get_avg_embs
